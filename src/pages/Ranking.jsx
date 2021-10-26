98b67a0e9d829981eaac3d55053a728476dc8dd0
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-10-26 16:02:48 -0300

    resolve requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">ranking page</h1>
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          data-testid="btn-go-home"
+        >
+          Voltar ao início
+        </button>
       </div>
     );
   }
 }
 
-export default Ranking;
+const mapStateToProps = () => ({});
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default connect(mapStateToProps)(Ranking);
